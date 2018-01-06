d5f6e7a5f76d8a530a3fcca7a4e7ac43728eaaa0
skim <caelum.modus@gmail.com>
2018-01-05 21:07:09 -0800

    initialized project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <form className='form-horizontal'>
+          <div className="col-lg-12">
+            <h3>San Diego Top Spots</h3>
+            <h5>A list of the top 30 places to see in San Diego, California.</h5>
+            <hr />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;

c165ecdd6f2b04d21a78986e830adc9e38da743a
skim <caelum.modus@gmail.com>
2018-01-10 21:09:19 -0800

    installed and imported axios and linked Google maps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TopSpot from './topspot';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
+  }
+
   render() {
     return (
       <div className='App'>
-        <form className='form-horizontal'>
-          <div className="col-lg-12">
-            <h3>San Diego Top Spots</h3>
-            <h5>A list of the top 30 places to see in San Diego, California.</h5>
-            <hr />
-          </div>
-        </form>
+        <header className="jumbotron">
+          <h1>San Diego Top Spots</h1>
+          <h5>A list of the top 30 places to see in San Diego, California.</h5>
+        </header>
+
+        { this.state.topspots.map(topspot => (
+          <TopSpot
+            key={topspot.id}
+            name={topspot.name}
+            description={topspot.description}
+            location={topspot.location} />
+        ))
+        }
+
       </div>
     );
   }
